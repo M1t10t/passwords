9df1ffb12a87481b6407abc3501db635ff52ec8a
Amin <aminzonisroilov27@gmail.com>
2024-08-17 12:51:28 +0500

    create password for free

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-numbers = int(input("Введите количество символов которые должны быть в пароле"))
+import random
 
+numbers = int(input("Введите количество символов которые должны быть в пароле: "))
+simbols="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+password=""
+for i in range(numbers):
+    password+=simbols[random.randint(0, len(simbols)-1)]
+print(password)
